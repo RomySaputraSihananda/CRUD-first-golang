749f1d2109c829e453b9b8992f2011aedc9728e4
romys <romysaputrasihanandaa@gmail.com>
2024-01-08 18:40:01 +0700

    update

--- main.go
+++ main.go
@@ -1,40 +1,21 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 
 	docs "app/docs"
+	services "app/libs/services"
 
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
-type Student struct {
-	Id int `json:"id"`
-	Name string `json:"name"`
-}
-
-var students []Student
-
 const (
 	PORT int = 4444
 	TAG string = "/api/v1"
 )
-
-// Ping godoc
-// @Summary Ping example
-// @Description Pings the server
-// @Tags example
-// @Produce json
-// @Success 200 {string} string "pong"
-// @Router /ping [get]
-func Ping(c *gin.Context) {
-    c.JSON(http.StatusOK, "pong")
-}
-
 
 func main() {
 	router := gin.Default()
@@ -46,39 +27,8 @@ func main() {
 		c.Redirect(http.StatusMovedPermanently, "/swagger-ui/index.html")
 	})
 
-	router.GET("/ping", Ping)
+	router.POST(fmt.Sprintf("%s/student", TAG), services.Create)
+	router.GET(fmt.Sprintf("%s/student", TAG), services.Read)
 
 	router.Run(fmt.Sprintf(":%d", PORT))
 }
-
-func handleStudents(w http.ResponseWriter, r *http.Request){
-	switch(r.Method){
-		case "GET":
-			getData(w, r)
-		case "POST":
-			addData(w, r)
-		case "PUT":
-			fmt.Print(r.Method)
-		case "DELETE":
-			fmt.Print(r.Method)
-		default:
-			fmt.Print(r.Method)
-	}
-}
-
-func getData(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(students)
-}
-
-func addData(w http.ResponseWriter, r *http.Request){
-	w.Header().Set("Content-Type", "application/json")
-	var newStudent Student
-
-	json.NewDecoder(r.Body).Decode(&newStudent)
-	newStudent.Id = len(students) + 1
-	students = append(students, newStudent)
-
-	json.NewEncoder(w).Encode(newStudent)
-}
-
